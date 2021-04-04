fa8796937349983c771e616773df83fc8631cd51
github username <github email address>
2021-04-04 13:15:55 -0400

    worked with the DOM and created elements using js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = `Donaldo is the champion`;
+
+document.appendChild(newHeader);
